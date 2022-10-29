4fb36f85a9b1ac0c3259bd318a6a142f883737c4
Kevin Blanco <kevinjblanco@gmail.com>
2022-10-29 19:08:26 -0400

    components and links created

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
 import './App.css'
+import Header from './components/Header'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import AddBook from './components/AddBook'
+import Books from './components/Books'
+import About from './components/About'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<AddBook />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom'
 const Header = () => {
 
   return (
-    <div>
+    <div className="header">
+      <nav className="headerLinks">
+      <Link to = {'/about'}>About Bookshelf</Link>
+        <Link to = {'/add'}>Add Book </Link>
+        <Link to = {'/books'}>Books</Link>
+      </nav>
     </div>
   )
 }
