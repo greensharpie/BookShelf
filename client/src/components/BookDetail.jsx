b7b64dde7c080075fd0157e541149b071a8cab66
Kevin Blanco <kevinjblanco@gmail.com>
2022-10-29 23:27:34 -0400

    update form created in book details

--- client/src/components/BookDetail.jsx
+++ client/src/components/BookDetail.jsx
@@ -27,7 +27,59 @@ const BookDetail = () => {
 
   return (
     <div>
-      
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>Name: </label>
+        <input 
+        id = 'name'
+        type = 'text'
+        value = {input.name}
+        onChange= {handleChange}
+        />
+        <br></br>
+        <label htmlFor='author'>Author: </label>
+        <input 
+        id= 'author'
+        type= 'text'
+        value = {input.author}
+        onChange= {handleChange}
+        />
+        <br></br>
+        <label htmlFor='description'>Description: </label>
+        <input 
+        id= 'description'
+        type= 'text'
+        value = {input.description}
+        onChange= {handleChange}
+        />
+        <br></br>
+        <label htmlFor='rating'>Rating: </label>
+        <input 
+        id= 'rating'
+        type= 'number'
+        value = {input.rating}
+        onChange= {handleChange}
+        />
+        <br></br>
+        <label htmlFor='available'>Available: </label>
+        <input 
+        id= 'available'
+        type= 'checkbox'
+        control = {input.checked}
+        value = {input.available}
+        onChange= {() => setChecked(!checked)}
+        />
+        <br></br>
+        <label htmlFor='image'>Image: </label>
+        <input 
+        id= 'image'
+        type= 'text'
+        value = {input.image}
+        onChange= {handleChange}
+        />
+        <br></br>
+        <button type='submit'>Update Book</button>
+        <br></br>
+      </form>
     </div>
   )
 }
