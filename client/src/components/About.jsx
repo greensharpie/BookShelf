8a9a927cecf5b418781093b6c0498892d2723201
Kevin Blanco <kevinjblanco@gmail.com>
2022-10-30 21:27:40 -0400

    about page added styling

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -3,7 +3,7 @@
 const About = () => {
   return (
     <div className="aboutContainer">
-      <div>
+      <div className="aboutDiv">
         <h2>Book Shelf</h2>
         <p>This CRUD app will help you keep track of your books.</p>
       </div>
