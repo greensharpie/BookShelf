5dd3418ce8de1a2c4a3fd40e505234e0ce9aed11
Kevin Blanco <kevinjblanco@gmail.com>
2023-01-30 15:48:10 -0500

    modification to db

--- db/index.js
+++ db/index.js
@@ -1,16 +1,39 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-let MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/booksDatabase'
+// let MONGODB_URI =
+//   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/booksDatabase'
+
+// mongoose
+//   .connect(MONGODB_URI)
+//   .then(() => {
+//     console.log('Successfully connected to MongoDB.')
+//   })
+//   .catch((e) => {
+//     console.error('Connection error', e.message)
+//   })
+
+// const db = mongoose.connection
+
+// module.exports = db
+
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/<database_name>'
+
+console.log('db ', dbUrl)
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(dbUrl)
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
   })
+
+mongoose.set('debug', true)
 
 const db = mongoose.connection
 
