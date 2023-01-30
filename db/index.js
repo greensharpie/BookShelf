ebda1a7881b190ee856ff671efadc33ce5d92f4e
Kevin Blanco <kevinjblanco@gmail.com>
2023-01-30 16:44:36 -0500

    updated config vars

--- db/index.js
+++ db/index.js
@@ -20,7 +20,7 @@ require('dotenv').config()
 let dbUrl =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI
-    : 'mongodb://127.0.0.1:27017/<database_name>'
+    : 'mongodb://127.0.0.1:27017/BookshelfCluster'
 
 console.log('db ', dbUrl)
 
